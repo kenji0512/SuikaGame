a160ae4344103ef854ce496054401cbc27526f94
DESKTOP-9FM1A4D\vantan <mjoni3899@gmail.com>
2024-10-19 14:36:39 +0900

    バグ修正

--- Assets/Scripts/FruitController.cs
+++ Assets/Scripts/FruitController.cs
@@ -18,30 +18,10 @@ public class FruitController : MonoBehaviour
                 {
                     Instantiate(_nextFruitPrefabs, this.transform.position, this.transform.rotation);
                 }
-                // 合成後に生成する果物をリストからランダムに選択
-                //GameObject nextFruitPrefab = GetRandomFruitPrefab();
-
-                //if (nextFruitPrefab != null)
-                //{
-                //    // 新しい果物を現在の位置に生成
-                //    Instantiate(nextFruitPrefab, transform.position, Quaternion.identity);
-                //}
-
                 // 元の2つの果物を破壊
                 Destroy(collision.gameObject);
                 Destroy(this.gameObject);
             }
         }
     }
-
-    // 合成後に生成される果物をリストからランダムに選択するメソッド
-    //private GameObject GetRandomFruitPrefab()
-    //{
-    //    if (_nextFruitPrefabs.Count > 0)
-    //    {
-    //        int randomIndex = Random.Range(0, _nextFruitPrefabs.Count);
-    //        return _nextFruitPrefabs[randomIndex];
-    //    }
-    //    return null; // リストが空の場合はnullを返す
-    //}
 }
